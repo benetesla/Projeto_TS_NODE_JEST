255c2f3cc8078ffdc44a2239f88fb060e92edd47
Bene-Tesla <benevaniosantos930@gmail.com>
2023-08-30 20:08:27 -0300

    :star: um arquivo de Enum contendo os valores de nossas question

---
+++ src/enum/choices-boilerplate-enum.ts
@@ -0,0 +1,13 @@
+/* eslint-disable prettier/prettier */
+export enum EChoicesBoil{
+    JAVASCRIPT = 'Javascript',
+    TYPESCRIPT = 'Typescript',
+    PYTHON = 'Python',
+    JAVA = 'Java',
+    CPP = 'C++',
+    NODEJS = 'NodeJS',
+    SASS = 'SASS',
+    Tawin = 'Tailwind',
+}
+
+//Javascript', 'Typescript', 'Python', 'Java', 'C++'
